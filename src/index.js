3f59692c848835522333ad35d4d7cdee59d2a4e6
sabrybabi <98984967+sabrybabi@users.noreply.github.com>
2022-03-20 19:35:51 +0100

    Fixed js

--- src/index.js
+++ src/index.js
@@ -101,18 +101,32 @@ function getData(event, parameter) {
   }
 
   function showTemperature(response) {
-    console.log("okay");
-
     let cityName = document.querySelector("h3");
     let inputCity = document.querySelector("#cityInput").value;
-    let splitInput = inputCity.split(" ");
-    cityName.innerHTML = splitInput
-    .map((word) => {
-      return word[0].toUpperCase() + word.substring(1);
-    })
-    .join(" ");
+    if (inputCity !== "") {
+      let splitInput = inputCity.split(" ");
+      cityName.innerHTML = splitInput
+      .map((word) => {
+        return word[0].toUpperCase() + word.substring(1);
+      })
+      .join(" ");
+    } else {
+      cityName.innerHTML = response.data.name;
+    }
     let temperatureValue = document.querySelector("#temperature-number");
     temperatureValue.innerHTML = Math.round(response.data.main.temp);
+
+    //substitute minor data
+    var minTemp = document.querySelector("#min-temp");
+    var maxTemp = document.querySelector("#max-temp");
+    minTemp.innerHTML = `${Math.round(response.data.main.temp_min)}°C`;
+    maxTemp.innerHTML = `${Math.round(response.data.main.temp_max)}°C`;
+
+    var windspeed = document.querySelector("#windspeed");
+    windspeed.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+
+    var humidity = document.querySelector("#humidity");
+    humidity.innerHTML = `${response.data.main.humidity}%`;
 
     //Search function - convert
 
@@ -124,7 +138,11 @@ function getData(event, parameter) {
     function celsiusToFarenheit(event) {
       event.preventDefault();
       let farenheitTemperature = Math.round((response.data.main.temp * 9) / 5 + 32);
+      let minTempFarenheit = Math.round((response.data.main.temp_min * 9) / 5 + 32);
+      let maxTempFarenheit = Math.round((response.data.main.temp_max * 9) / 5 + 32);
       temperatureValue.innerHTML = farenheitTemperature;
+      minTemp.innerHTML = `${minTempFarenheit}°F`;
+      maxTemp.innerHTML = `${maxTempFarenheit}°F`;
       farenheitSymbol.classList.remove("inactive-link");
       farenheitSymbol.classList.add("active-link");
       celsiusSymbol.classList.remove("active-link");
@@ -134,43 +152,34 @@ function getData(event, parameter) {
     function farenheitToCelsius(event) {
       event.preventDefault();
       temperatureValue.innerHTML = Math.round(response.data.main.temp);
+      minTemp.innerHTML = `${Math.round(response.data.main.temp_min)}°C`;
+      maxTemp.innerHTML = `${Math.round(response.data.main.temp_max)}°C`;
       celsiusSymbol.classList.remove("inactive-link");
       celsiusSymbol.classList.add("active-link");
       farenheitSymbol.classList.remove("active-link");
       farenheitSymbol.classList.add("inactive-link");
     }
 
-    //substitute minor data
-    let minTemp = document.querySelector("#min-temp");
-    let maxTemp = document.querySelector("#max-temp");
-    minTemp.innerHTML = `${Math.round(response.data.main.temp_min)}°C`;
-    maxTemp.innerHTML = `${Math.round(response.data.main.temp_max)}°C`;
-
-    let windspeed = document.querySelector("#windspeed");
-    windspeed.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
-
-    let humidity = document.querySelector("#humidity");
-    humidity.innerHTML = `${response.data.main.humidity}%`;
-
+    
     if (response.data.weather[0].main === ["Drizzle", "Rain"].indexOf()) {
-    document.body.style.backgroundImage = "url('sfondo-rainy.4257b76d.png')";
-    document.querySelector("#mainIcon").src = "rain.1e042ddf.png";
+    document.body.style.backgroundImage = "url('src/images/sfondo-rainy.png')";
+    document.querySelector("#mainIcon").src = "src/images/rain.png";
     }
     if (response.data.weather[0].main === "Thunderstorm") {
-      document.body.style.backgroundImage = "url('sfondo-thunder.4cf0b0bf.png')";
-      document.querySelector("#mainIcon").src = "thunderstorm.b65ce3a6.png";
+      document.body.style.backgroundImage = "url('src/images/sfondo-thunder.png')";
+      document.querySelector("#mainIcon").src = "src/images/thunderstorm.png";
     }
     if (response.data.weather[0].main === "Snow") {
-      document.body.style.backgroundImage = "url('sfondo-snowy.35f42c3a.png')";
-      document.querySelector("#mainIcon").src = "snow.f87c4275.png";
+      document.body.style.backgroundImage = "url('src/images/sfondo-snowy.png')";
+      document.querySelector("#mainIcon").src = "src/images/snow.png";
     }
     if (response.data.weather[0].main === "Clear") {
-      document.body.style.backgroundImage = "url('sfondo-sunny.55ad7263.png')";
-      document.querySelector("#mainIcon").src = "sun.328582e4.png";
+      document.body.style.backgroundImage = "url('src/images/sfondo-sunny.png')";
+      document.querySelector("#mainIcon").src = "src/images/sun.png";
     }
     if (response.data.weather[0].main === "Clouds") {
-      document.body.style.backgroundImage = "url('sfondo-cloudy.267b0bf8.png')";
-      document.querySelector("#mainIcon").src = "clouds.8c9e0a75.png";
+      document.body.style.backgroundImage = "url('src/images/sfondo-cloudy.png')";
+      document.querySelector("#mainIcon").src = "src/images/clouds.png";
     }
     if (
       response.data.weather[0].description === "broken clouds" ||
@@ -178,8 +187,47 @@ function getData(event, parameter) {
       response.data.weather[0].description === "few clouds"
     ) {
       document.body.style.backgroundImage =
-        "url('sfondo-sun-cloudy.9e0bdc4f.png')";
-      document.querySelector("#mainIcon").src = "cloudy.17aa9d1b.png";
+        "url('src/images/sfondo-sun-cloudy.png')";
+      document.querySelector("#mainIcon").src = "src/images/cloudy.png";
     }
+
+    //reset input value
+    document.querySelector("#cityInput").value = "";
+  }
+
+  function showTemperatureSmall(response) {
+    for (let i = 1; i < response.data.data.length; i++) {
+    let temperatureLocale = document.querySelector("#temperature-day-" + i);
+    temperatureLocale.innerHTML = Math.round(response.data.data[i].temp) + "°C";
+
+    let weatherConditionLocale = response.data.data[i].weather.code;
+    if (weatherConditionLocale >= 200 && weatherConditionLocale < 300) {
+      document.querySelector("#small-icon-" + i).src = "src/images/thunderstorm.png";
+    }
+    if (weatherConditionLocale >= 300 && weatherConditionLocale < 600) {
+      document.querySelector("#small-icon-" + i).src = "src/images/rain.png";
+    }
+    if (weatherConditionLocale >= 600 && weatherConditionLocale < 700) {
+      document.querySelector("#small-icon-" + i).src = "src/images/snow.png";
+    }
+    if (weatherConditionLocale >= 700 && weatherConditionLocale < 800 || weatherConditionLocale === 804) {
+      document.querySelector("#small-icon-" + i).src = "src/images/clouds.png";
+    }
+    if (weatherConditionLocale === 800) {
+      document.querySelector("#small-icon-" + i).src = "src/images/sun.png";
+    }
+    if (weatherConditionLocale >= 801 && weatherConditionLocale < 804) {
+      document.querySelector("#small-icon-" + i).src = "src/images/cloudy.png";
+    }
+
+    let forecastDays = document.querySelector("#day-" + i);
+    let dateTime = response.data.data[i].datetime;
+
+    forecastDays.innerHTML = new Date(dateTime).toLocaleString('en-us', {weekday: "short"});
+
   }
 }
+}
+window.onload = function() {
+  document.getElementById("currentLocation").click();
+};
